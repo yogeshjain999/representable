40864b84bf877b29e7882c2ae8e49662430671fe
Nick Sutterer <apotonick@gmail.com>
2012-05-31 10:53:41 +0200

    remove it with 1.3 ;)

--- lib/representable/deprecations.rb
+++ lib/representable/deprecations.rb
@@ -1,8 +1,8 @@
 module Representable::Deprecations
-  def skip_excluded_property?(binding, options) # TODO: remove with 1.2.
+  def skip_excluded_property?(binding, options) # TODO: remove with 1.3.
     if options[:except]
       options[:exclude] = options[:except]
-      warn "The :except option is deprecated and will be removed in 1.2. Please use :exclude."
+      warn "The :except option is deprecated and will be removed in 1.3. Please use :exclude."
     end # i wanted a one-liner but failed :)
     super
   end
