5f4ee1f66985eb25819177bc675ff6a398ce4607
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-06-20 19:41:11 -0300

    Fixing ruby 1.8 compatibility. And changing tests to ignore ordering of generated json.

--- lib/representable/json/hash.rb
+++ lib/representable/json/hash.rb
@@ -4,24 +4,24 @@ module Representable::JSON
   module Hash
     include Representable::JSON
     include HashMethods
-    
+
     def self.included(base)
       base.class_eval do
         include Representable
         extend ClassMethods
       end
     end
-    
-    
+
+
     module ClassMethods
       def values(options)
         hash :_self, options
       end
     end
-    
-    
+
+
     def definition_opts
-      [:_self, :hash => true, :use_attributes => true]
+      [:_self, { :hash => true, :use_attributes => true }]
     end
   end
 end
--- lib/representable/xml/hash.rb
+++ lib/representable/xml/hash.rb
@@ -4,48 +4,48 @@ module Representable::XML
   module AttributeHash
     include Representable::XML
     include Representable::HashMethods
-    
+
     def self.included(base)
       base.class_eval do
         include Representable
         extend ClassMethods
       end
     end
-    
-    
+
+
     module ClassMethods
       def values(options)
         hash :_self, options.merge!(:use_attributes => true)
       end
     end
-    
-    
+
+
     def definition_opts
-      [:_self, :hash => true, :use_attributes => true]
+      [:_self, { :hash => true, :use_attributes => true }]
     end
   end
-  
+
   module Hash
     include Representable::XML
     include HashMethods
-    
+
     def self.included(base)
       base.class_eval do
         include Representable
         extend ClassMethods
       end
     end
-    
-    
+
+
     module ClassMethods
       def values(options)
         hash :_self, options
       end
     end
-    
-    
+
+
    def definition_opts
-      [:_self, :hash => true]
+      [:_self, { :hash => true }]
     end
   end
 end
