7219aac93cc8be4b8d3eba6835383293d88c5092
Nick Sutterer <apotonick@gmail.com>
2012-11-01 03:01:18 +0100

    enhancing test.

--- test/coercion_test.rb
+++ test/coercion_test.rb
@@ -10,12 +10,12 @@ class VirtusCoercionTest < MiniTest::Spec
       module SongRepresenter
         include Representable::JSON
         include Representable::Coercion
-        property :composed_at, :type => DateTime
-        property :track, :type => Integer
+        property :composed_at,  :type => DateTime
+        property :track,        :type => Integer
       end
      
       it "coerces properties in #from_json" do
-        song = Song.new.extend(SongRepresenter).from_json("{\"composed_at\":\"November 18th, 1983\",\"track\":\"18\",}")
+        song = Song.new.extend(SongRepresenter).from_json("{\"composed_at\":\"November 18th, 1983\",\"track\":\"18\"}")
         assert_kind_of DateTime, song.composed_at
         assert_equal 18, song.track
         assert_equal DateTime.parse("Fri, 18 Nov 1983 00:00:00 +0000"), song.composed_at
@@ -23,23 +23,28 @@ class VirtusCoercionTest < MiniTest::Spec
     end
     
     
-    class ImmigrantSong
-      include Representable::JSON
-      include Virtus
-      include Representable::Coercion
+    describe "on class level" do
+      class ImmigrantSong
+        attr_accessor :track
+        include Representable::JSON
+        include Virtus
+        include Representable::Coercion
+        
+        property :composed_at,  :type => DateTime, :default => "May 12th, 2012"
+        property :track,        :type => Integer
+      end
       
-      property :composed_at, :type => DateTime, :default => "May 12th, 2012"
-    end
-    
-    it "coerces into the provided type" do
-      song = ImmigrantSong.new.from_json("{\"composed_at\":\"November 18th, 1983\"}")
-      assert_equal DateTime.parse("Fri, 18 Nov 1983 00:00:00 +0000"), song.composed_at
-    end
-    
-    it "respects the :default options" do
-      song = ImmigrantSong.new.from_json("{}")
-      assert_kind_of DateTime, song.composed_at
-      assert_equal DateTime.parse("Mon, 12 May 2012 00:00:00 +0000"), song.composed_at
+      it "coerces into the provided type" do
+        song = ImmigrantSong.new.from_json("{\"composed_at\":\"November 18th, 1983\",\"track\":\"18\"}")
+        assert_equal DateTime.parse("Fri, 18 Nov 1983 00:00:00 +0000"), song.composed_at
+        assert_equal 18, song.track
+      end
+      
+      it "respects the :default options" do
+        song = ImmigrantSong.new.from_json("{}")
+        assert_kind_of DateTime, song.composed_at
+        assert_equal DateTime.parse("Mon, 12 May 2012 00:00:00 +0000"), song.composed_at
+      end
     end
     
   end
