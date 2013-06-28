d06e979cce9e2955a66e4d76f3a0fdbb2759c098
Nick Sutterer <apotonick@gmail.com>
2013-06-28 11:53:47 +1000

    fix whitespaces.

--- lib/representable/xml.rb
+++ lib/representable/xml.rb
@@ -11,12 +11,12 @@ module Representable
         self.representation_wrap = true # let representable compute it.
       end
     end
-    
-    
+
+
     module ClassMethods
       # Creates a new Ruby object from XML using mapping information declared in the class.
       #
-      # Accepts a block yielding the currently iterated Definition. If the block returns false 
+      # Accepts a block yielding the currently iterated Definition. If the block returns false
       # the property is skipped.
       #
       # Example:
@@ -24,29 +24,29 @@ module Representable
       def from_xml(*args, &block)
         create_represented(*args, &block).from_xml(*args)
       end
-      
+
       def from_node(*args, &block)
         create_represented(*args, &block).from_node(*args)
       end
     end
-    
-    
+
+
     def from_xml(doc, *args)
       node = Nokogiri::XML(doc).root
       from_node(node, *args)
     end
-    
+
     def from_node(node, options={})
       update_properties_from(node, options, PropertyBinding)
     end
-    
+
     # Returns a Nokogiri::XML object representing this object.
     def to_node(options={})
       root_tag = options[:wrap] || representation_wrap
-      
+
       create_representation_with(Nokogiri::XML::Node.new(root_tag.to_s, Nokogiri::XML::Document.new), options, PropertyBinding)
     end
-    
+
     def to_xml(*args)
       to_node(*args).to_s
     end
