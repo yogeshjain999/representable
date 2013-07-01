3915c98ea4f181c1072bcec50bc1c1a0c5f1f2c5
Nick Sutterer <apotonick@gmail.com>
2013-07-01 14:38:13 +1000

    change the way coercion works: we now have a dedicated virtus Coercion instance to do the conversion.

--- lib/representable/decorator/coercion.rb
+++ lib/representable/decorator/coercion.rb
@@ -4,55 +4,20 @@ class Representable::Decorator
   module Coercion
     def self.included(base)
       base.class_eval do
-        # DISCUSS: this assumes we have a Representer included, yet.
-        alias_method :representable_initialize, :initialize
-        alias_method :representable_to_hash,    :to_hash
-
-        # FIXME: allow including coercion only from virtus.
-        include Virtus
-        undef_method(:initialize)
-        undef_method(:to_hash)
-
-        extend Representable::Coercion::ClassMethods
+        include Representable::Coercion
         extend ClassMethods
-
-        def initialize(*args) # override Virtus' #initialize.
-          representable_initialize(*args)
-        end
-
-        def to_hash(*args) # override Virtus' #to_hash.
-          representable_to_hash(*args)
-        end
       end
     end
 
     module ClassMethods
       def property(name, options={})
         if options[:type]
-          options[:decorator_scope] = true # call setter on decorator so coercion kicks in.
-          create_writer(name)
-          create_reader(name)
+          options[:decorator_scope] = true
+          options[:getter] = lambda { |*| coercer.coerce(name, decorated.send(name)) }
+          options[:setter] = lambda { |v,*| decorated.send("#{name}=", coercer.coerce(name, v)) }
         end
 
         super # Representable::Coercion.
-      end
-
-    private
-      # FIXME: dear @solnic, please make this better!
-      def create_writer(name)
-        # the call to super makes the actual coercion, which is then delegated to the represented instance.
-        define_method "#{name}=" do |v|
-          coerced_value = super(v).get(self)
-          represented.send("#{name}=", coerced_value)
-        end
-      end
-
-      def create_reader(name)
-        # the call to super makes the actual coercion, which is then delegated to the represented instance.
-        define_method "#{name}" do
-          send("#{name}=", represented.send(name))
-          super()
-        end
       end
     end
   end
--- test/coercion_test.rb
+++ test/coercion_test.rb
@@ -3,9 +3,11 @@ require 'representable/coercion'
 require 'representable/decorator/coercion'
 
 class VirtusCoercionTest < MiniTest::Spec
-  class Song  # note that we don't define accessors for the properties here.
-    attr_accessor :title
+  class Song  # note that we don't define accessors for the properties here. ### FIXME
+    attr_accessor :title, :composed_at
   end
+
+  let (:date) { DateTime.parse("Fri, 18 Nov 1983 00:00:00 +0000") }
 
   describe "Coercion with Virtus" do
     describe "on object level" do
@@ -20,56 +22,58 @@ class VirtusCoercionTest < MiniTest::Spec
       it "coerces properties in #from_json" do
         song = Song.new.extend(SongRepresenter).from_json('{"composed_at":"November 18th, 1983","track":"18","title":"Scarified"}')
         assert_kind_of DateTime, song.composed_at
-        assert_equal DateTime.parse("Fri, 18 Nov 1983 00:00:00 +0000"), song.composed_at
+        assert_equal date, song.composed_at
         assert_equal 18, song.track
         song.title.must_equal "Scarified"
       end
 
-      # it "coerces when rendering" do
-      #   song = Song.new
-      #   song.title = "Scarified"
-      #   song.to_json.must_equal ''
-      # end
+       it "coerces when rendering" do
+         song = Song.new.extend(SongRepresenter)
+         song.title       = "Scarified"
+         song.composed_at = "Fri, 18 Nov 1983"
+
+         song.to_hash.must_equal({"title" => "Scarified", "composed_at" => date})
+       end
     end
 
 
-    describe "on class level" do
-      class ImmigrantSong
-        include Representable::JSON
-        include Representable::Coercion
+     describe "on class level" do
+       class ImmigrantSong
+         include Representable::JSON
+         include Representable::Coercion
 
-        property :composed_at,  :type => DateTime, :default => "May 12th, 2012"
-        property :track,        :type => Integer
-      end
+         property :composed_at,  :type => DateTime, :default => "May 12th, 2012"
+         property :track,        :type => Integer
+       end
 
-      it "coerces into the provided type" do
-        song = ImmigrantSong.new.from_json("{\"composed_at\":\"November 18th, 1983\",\"track\":\"18\"}")
-        assert_equal DateTime.parse("Fri, 18 Nov 1983 00:00:00 +0000"), song.composed_at
-        assert_equal 18, song.track
-      end
+       it "coerces into the provided type" do
+         song = ImmigrantSong.new.from_json("{\"composed_at\":\"November 18th, 1983\",\"track\":\"18\"}")
+         assert_equal date, song.composed_at
+         assert_equal 18, song.track
+       end
 
-      it "respects the :default options" do
-        song = ImmigrantSong.new.from_json("{}")
-        assert_kind_of DateTime, song.composed_at
-        assert_equal DateTime.parse("Mon, 12 May 2012 00:00:00 +0000"), song.composed_at
-      end
-    end
+       it "respects the :default options" do
+         song = ImmigrantSong.new.from_json("{}")
+         assert_kind_of DateTime, song.composed_at
+         assert_equal DateTime.parse("Mon, 12 May 2012 00:00:00 +0000"), song.composed_at
+       end
+     end
 
-    describe "on decorator" do
-      class SongRepresentation < Representable::Decorator
-        include Representable::JSON
-        include Representable::Decorator::Coercion
+     describe "on decorator" do
+       class SongRepresentation < Representable::Decorator
+         include Representable::JSON
+         include Representable::Decorator::Coercion
 
-        property :composed_at, :type => DateTime
-        property :title
-      end
+         property :composed_at, :type => DateTime
+         property :title
+       end
 
-      it "coerces when parsing" do
-        song = SongRepresentation.new(OpenStruct.new).from_json("{\"composed_at\":\"November 18th, 1983\", \"title\": \"Scarified\"}")
-        song.must_be_kind_of OpenStruct
-        song.composed_at.must_equal DateTime.parse("Fri, 18 Nov 1983")
-        song.title.must_equal "Scarified"
-      end
+       it "coerces when parsing" do
+         song = SongRepresentation.new(OpenStruct.new).from_json("{\"composed_at\":\"November 18th, 1983\", \"title\": \"Scarified\"}")
+         song.must_be_kind_of OpenStruct
+         song.composed_at.must_equal date
+         song.title.must_equal "Scarified"
+       end
 
       it "coerces when rendering" do
         SongRepresentation.new(
