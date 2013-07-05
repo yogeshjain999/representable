e2726b06cbf6fcfaaad60b3f04df4d4530aac61f
Nick Sutterer <apotonick@gmail.com>
2013-07-05 11:17:26 +1000

    simplify coercion and fix an "inheritance bug".

--- lib/representable/coercion.rb
+++ lib/representable/coercion.rb
@@ -13,27 +13,19 @@ module Representable::Coercion
   # separate coercion object doesn't give us initializer and accessors in the represented object (with module representer)!
 
   def self.included(base)
-    base.class_eval do
-      extend ClassMethods
-
-      representable_attrs.instance_eval do
-        def coercer_class
-          @coercer_class ||= Class.new(Coercer)
-        end
-      end
-    end
+    base.extend ClassMethods
   end
 
   module ClassMethods
     def property(name, args={})
       return super unless args[:type]
 
-      representable_attrs.coercer_class.instance_eval do
-        attribute(name, args[:type])
-      end
+      representable_attrs.coercer_class.attribute(name, args[:type])
 
       # DISCUSS: we add accessors here, basically to provide the same API as before. also, we don't wanna override :getter here, do we?
       # TODO: deprecate these accessors for 1.7 and use coercer.coerce here, too.
+
+      # FIXME: does that actually store the new value in the represented ?
       define_method(name) do
         coercer.send(name)
       end
@@ -42,6 +34,15 @@ module Representable::Coercion
       end
 
       super
+    end
+
+    def representable_attrs
+      super.instance_eval do
+        def coercer_class
+          @coercer_class ||= Class.new(Coercer)
+        end
+        self
+      end
     end
   end
 
