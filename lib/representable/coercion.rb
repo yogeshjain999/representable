3c8cf1f08439206bf349c411f23c122e5ec8f4be
Nick Sutterer <apotonick@gmail.com>
2013-07-05 15:36:40 +1000

    coercion now works by storing the Coercion instance in a inheritable attribute in Config.

--- lib/representable/coercion.rb
+++ lib/representable/coercion.rb
@@ -13,14 +13,19 @@ module Representable::Coercion
   # separate coercion object doesn't give us initializer and accessors in the represented object (with module representer)!
 
   def self.included(base)
-    base.extend ClassMethods
+    base.class_eval do
+      extend ClassMethods
+      # FIXME: use inheritable_attr when it's ready.
+      representable_attrs.inheritable_array(:coercer_class) << Class.new(Coercer) unless representable_attrs.inheritable_array(:coercer_class).first
+    end
+
   end
 
   module ClassMethods
     def property(name, args={})
       return super unless args[:type]
 
-      representable_attrs.coercer_class.attribute(name, args[:type])
+      representable_attrs.inheritable_array(:coercer_class).first.attribute(name, args[:type])
 
       # DISCUSS: we add accessors here, basically to provide the same API as before. also, we don't wanna override :getter here, do we?
       # TODO: deprecate these accessors for 1.7 and use coercer.coerce here, too.
@@ -35,18 +40,9 @@ module Representable::Coercion
 
       super
     end
-
-    def representable_attrs
-      super.instance_eval do
-        def coercer_class
-          @coercer_class ||= Class.new(Coercer)
-        end
-        self
-      end
-    end
   end
 
   def coercer
-    @coercer ||= representable_attrs.coercer_class.new
+    @coercer ||= representable_attrs.inheritable_array(:coercer_class).first.new
   end
 end
