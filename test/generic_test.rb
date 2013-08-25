ff23dde06b0fd72e6a32b78269b830539ff78bfc
Nick Sutterer <apotonick@gmail.com>
2013-08-25 19:38:53 +1000

    adding Test::for_formats so the "meta tests" for different formats gets its own place. adding tests for all formats for property :sync'ing.

--- test/generic_test.rb
+++ test/generic_test.rb
@@ -30,7 +30,7 @@ class GenericTest < MiniTest::Spec
       property :song, :instance => lambda { |*| nil }, :extend => song_representer
     end
 
-    let (:hit) { hit = OpenStruct.new(:song => song).extend(representer) } # note that song is already representable.
+    let (:hit) { hit = OpenStruct.new(:song => song).extend(representer) }
 
     it "calls #to_hash on song instance, nothing else" do
       hit.to_hash.must_equal("song"=>{"title"=>"Resist Stance"})
@@ -44,37 +44,57 @@ class GenericTest < MiniTest::Spec
     end
   end
 
-
-  describe "property with parse_strategy: :sync" do # TODO: introduce :representable option?
-    representer!(:inject => :song_representer) do
-      property :song, :parse_strategy => :sync, :extend => song_representer
+  def self.for_formats(formats)
+    formats.each do |format, cfg|
+      mod, output, input = cfg
+      yield format, mod, output, input
     end
+  end
 
-    let (:hit) { hit = OpenStruct.new(:song => song).extend(representer) } # note that song is already representable.
 
-    it "calls #to_hash on song instance, nothing else" do
-      hit.to_hash.must_equal("song"=>{"title"=>"Resist Stance"})
-    end
+  for_formats(
+    :hash => [Representable::Hash, {"song"=>{"title"=>"Resist Stance"}}, {"song"=>{"title"=>"Suffer"}}],
+    :xml  => [Representable::XML, "<open_struct><song><title>Resist Stance</title></song></open_struct>", "<open_struct><song><title>Suffer</title></song></open_struct>",],
+    :yaml => [Representable::YAML, "---\nsong:\n  title: Resist Stance\n", "---\nsong:\n  title: Suffer\n"],
+  ) do |format, mod, output, input|
 
-    it "calls #from_hash on the existing song instance, nothing else" do
-      song_id = hit.song.object_id
+    describe "[#{format}] property with parse_strategy: :sync" do # TODO: introduce :representable option?
+      let (:format) { format }
 
-      hit.from_hash("song"=>{"title"=>"Suffer"})
+      representer!(:module => mod, :name => :song_representer) do
+        property :title
+        self.representation_wrap = :song if format == :xml
+      end
 
-      hit.song.title.must_equal "Suffer"
-      hit.song.object_id.must_equal song_id
+      representer!(:inject => :song_representer, :module => mod) do
+        property :song, :parse_strategy => :sync, :extend => song_representer
+      end
+
+      let (:hit) { hit = OpenStruct.new(:song => song).extend(representer) }
+
+      it "calls #to_hash on song instance, nothing else" do
+        render(hit).must_equal_document(output)
+      end
+
+
+      it "calls #from_hash on the existing song instance, nothing else" do
+        song_id = hit.song.object_id
+
+        parse(hit, input)
+
+        hit.song.title.must_equal "Suffer"
+        hit.song.object_id.must_equal song_id
+      end
     end
   end
 
   # FIXME: there's a bug with XML and the collection name!
-  {
+  for_formats(
     :hash => [Representable::Hash, {"songs"=>[{"title"=>"Resist Stance"}]}, {"songs"=>[{"title"=>"Suffer"}]}],
     #:json => [Representable::JSON, "{\"song\":{\"name\":\"Alive\"}}", "{\"song\":{\"name\":\"You've Taken Everything\"}}"],
     :xml  => [Representable::XML, "<open_struct><song><title>Resist Stance</title></song></open_struct>", "<open_struct><songs><title>Suffer</title></songs></open_struct>"],
     :yaml => [Representable::YAML, "---\nsongs:\n- title: Resist Stance\n", "---\nsongs:\n- title: Suffer\n"],
-  }.each do |format, cfg|
-    mod, output, input = cfg
-
+  ) do |format, mod, output, input|
 
     describe "[#{format}] collection with :parse_strategy: :sync" do # TODO: introduce :representable option?
       let (:format) { format }
