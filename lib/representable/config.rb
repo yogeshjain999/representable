2df46eee4e982bd875ae351d4e6bb0f32e2a603a
Nick Sutterer <apotonick@gmail.com>
2013-08-29 14:44:09 +1000

    override properties instead of adding them again. warning - if you relied on the former behaviour for whatever reason, this will break code.

--- lib/representable/config.rb
+++ lib/representable/config.rb
@@ -1,6 +1,6 @@
 module Representable
   # NOTE: the API of Config is subject to change so don't rely too much on this private object.
-  class Config < Array
+  class Config < Array # TODO: change to Hash.
     # DISCUSS: experimental. this will soon be moved to a separate gem
     module InheritableArray
       def inheritable_array(name)
@@ -17,6 +17,30 @@ module Representable
           inheritable_array(k).push *parent.inheritable_array(k).clone
         end
       end
+    end
+
+    # def <<(definition)
+    #   self[definition.name] = definition
+    #   self
+    # end
+    # def last
+    #   self.values.last
+    # end
+    # def push(config={}) # FIXME: why do we need a default arg here?
+    #   puts "merging: #{config.inspect} with self: #{self.inspect}"
+    #   merge!(config)
+    # end
+    # def each(&block)
+    #   values.each(&block)
+    # end
+
+    def <<(definition)
+      if overridden = find { |d| d.name == definition.name }
+        self[index(overridden)] = definition
+        return self
+      end
+
+      super(definition)
     end
 
 
@@ -42,6 +66,10 @@ module Representable
     include InheritableArray # overrides #inherit.
 
   private
+    def push(*defs)
+      defs.each { |d| self << d }
+    end
+
     def infer_name_for(name)
       name.to_s.split('::').last.
        gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
