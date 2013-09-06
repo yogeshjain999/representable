1334977c05cf82241ae9a5148d6659736006c1f3
Nick Sutterer <apotonick@gmail.com>
2013-09-06 15:04:25 +1000

    Serializer now performs the check for typed?.

--- lib/representable/serializer.rb
+++ lib/representable/serializer.rb
@@ -2,7 +2,7 @@ require "representable/deserializer"
 
 module Representable
   class ObjectSerializer < ObjectDeserializer
-    def call
+    def call # TODO: make typed? switch here!
       return @object if @object.nil?
 
       representable = prepare(@object)
@@ -12,6 +12,9 @@ module Representable
 
   private
     def serialize(object, user_options)
+      # TODO: this used to be handled in #serialize where Object added it's behaviour. treat scalars as objects to remove this switch:
+      return object unless @binding.typed?
+
       object.send(@binding.serialize_method, user_options.merge!({:wrap => false}))
     end
   end
