bfbafb951be2374cf4d4e2f86a2f57febaf8ba08
Nick Sutterer <apotonick@gmail.com>
2014-03-21 14:58:36 +1100

    add :instance test that demonstrates how to find models instead of creating them.

--- test/instance_test.rb
+++ test/instance_test.rb
@@ -1,6 +1,29 @@
 require 'test_helper'
 
 class InstanceTest < GenericTest
+  Song = Struct.new(:id, :title)
+  Song.class_eval do
+    def self.find(id)
+      Song.new(2, "Invincible")
+    end
+  end
+
+# TODO: use *args in from_hash.
+# DISCUSS: do we need parse_strategy?
+  describe "property with :instance" do
+    representer!(:inject => :song_representer) do
+      property :song,
+        :instance => lambda { |fragment| fragment["id"] == song.id ? nil : Song.find(fragment["id"]) },
+        :extend => song_representer
+    end
+
+    it { OpenStruct.new(:song => Song.new(1, "The Answer Is Still No")).extend(representer).
+      from_hash("song" => {"id" => 1}).song.must_equal Song.new(1, "The Answer Is Still No") }
+
+    it { OpenStruct.new(:song => Song.new(1, "The Answer Is Still No")).extend(representer).
+      from_hash("song" => {"id" => 2}).song.must_equal Song.new(2, "Invincible") }
+  end
+
   describe "property with instance: { nil }" do # TODO: introduce :representable option?
     representer!(:inject => :song_representer) do
       property :song, :instance => lambda { |*| nil }, :extend => song_representer
