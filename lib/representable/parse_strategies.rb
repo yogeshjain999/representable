19b7237d8dddbfe9cf550f1f829f2b9d486dca1f
Nick Sutterer <apotonick@gmail.com>
2014-03-25 15:58:38 +1100

    start implementing :sync.

--- lib/representable/parse_strategies.rb
+++ lib/representable/parse_strategies.rb
@@ -2,15 +2,17 @@ module Representable
   module ParseStrategy
     def property(name, *args, &block)
       if options = args.first and strategy = options[:parse_strategy]
-        Sync.call(options)
+        Sync.call(name, options)
       end
 
       super
     end
 
     class Sync
-      def self.call(options)
-        options[]
+      def self.call(name, options)
+        options[:setter] = lambda { |*| }
+        return options[:instance] = lambda { |fragment,i,*| send(name)[i] } if options[:collection]
+        options[:instance] = lambda { |fragment,*| send(name) }
       end
     end
   end
