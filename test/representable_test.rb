809d8c4e15d78cd8284f65b22f749acd8b953cf2
Nick Sutterer <apotonick@gmail.com>
2014-04-02 10:36:59 +1100

    extract tests to ReaderWriterTest.

--- test/representable_test.rb
+++ test/representable_test.rb
@@ -486,24 +486,6 @@ class RepresentableTest < MiniTest::Spec
   end
 
 
-  describe ":reader and :writer" do
-    representer! do
-      property :name,
-        :writer => lambda { |doc, args| doc["title"] = "#{args[:nr]}) #{name}" },
-        :reader => lambda { |doc, *args| self.name = doc["title"].split(") ").last }
-    end
-
-    subject { OpenStruct.new(:name => "Disorder And Disarray").extend(representer) }
-
-    it "uses :writer when rendering" do
-      subject.to_hash(:nr => 14).must_equal({"title" => "14) Disorder And Disarray"})
-    end
-
-    it "uses :reader when parsing" do
-      subject.from_hash({"title" => "15) The Wars End"}).name.must_equal "The Wars End"
-    end
-  end
-
   describe ":extend and :class" do
     module UpcaseRepresenter
       include Representable
