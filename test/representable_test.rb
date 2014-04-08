8cc2fc5d774cd63835ef66a005277de0a551026e
Nick Sutterer <apotonick@gmail.com>
2014-04-09 08:21:41 +1000

    extract DecoratorScopeTest,ready for removal.

--- test/representable_test.rb
+++ test/representable_test.rb
@@ -598,31 +598,6 @@ class RepresentableTest < MiniTest::Spec
       end
     end
 
-    describe ":decorator_scope" do
-      representer! do
-        property :title, :getter => lambda { |*| title_from_representer }, :decorator_scope => true
-      end
-
-      let (:representer_with_method) {
-        Module.new do
-          include Representable::Hash
-          property :title, :decorator_scope => true
-          def title; "Crystal Planet"; end
-        end
-       }
-
-      it "executes lambdas in represented context" do
-        Class.new do
-          def title_from_representer
-            "Sounds Of Silence"
-          end
-        end.new.extend(representer).to_hash.must_equal({"title"=>"Sounds Of Silence"})
-      end
-
-      it "executes method in represented context" do
-        Object.new.extend(representer_with_method).to_hash.must_equal({"title"=>"Crystal Planet"})
-      end
-    end
 
     # TODO: Move to global place since it's used twice.
     class SongRepresentation < Representable::Decorator
