5ef3590fdfad7cf06a906b282dc91465865875a4
Nick Sutterer <apotonick@gmail.com>
2014-07-24 20:36:43 +1000

    add tests for Representable::Collection.

--- test/for_collection_test.rb
+++ test/for_collection_test.rb
@@ -71,3 +71,90 @@ class ForCollectionTest < MiniTest::Spec
 
   # with module including module
 end
+
+
+class ImplicitCollectionTest < MiniTest::Spec
+  let (:songs) { [song, Song.new("Can't Take Them All")] }
+  let (:song) { Song.new("Days Go By") }
+
+  for_formats(
+    :hash => [Representable::Hash, out=[{"name" => "Days Go By"}, {"name"=>"Can't Take Them All"}], out],
+    :json => [Representable::JSON, out="[{\"name\":\"Days Go By\"},{\"name\":\"Can't Take Them All\"}]", out],
+    # :xml  => [Representable::XML,  out="<a><song></song><song></song></a>", out]
+  ) do |format, mod, output, input|
+
+    # collection is automatically detected.
+    describe "Module#to_/from_#{format}" do
+      let (:format) { format }
+
+      let (:representer) {
+        Module.new do
+          include mod
+          include Representable::Collection
+          property :name
+        end
+      }
+
+      # rendering works out of the box, no config necessary
+      it { render(songs.extend(representer)).must_equal_document output }
+      it { parse([].extend(representer), input).must_equal songs }
+    end
+
+
+    # describe "Decorator::for_collection [#{format}]" do
+    #   let (:format) { format }
+    #   let (:representer) {
+    #     Class.new(Representable::Decorator) do
+    #       include mod
+    #       property :name
+
+    #       collection_representer :class => Song
+    #     end
+    #   }
+
+    #   it { render(representer.for_collection.new(songs)).must_equal_document output }
+    #   it { parse(representer.for_collection.new([]), input).must_equal songs }
+    # end
+  end
+
+
+  for_formats(
+    :hash => [Representable::Hash, out={"name" => "Days Go By"}, out],
+    :json => [Representable::JSON, out="[{\"name\":\"Days Go By\"},{\"name\":\"Can't Take Them All\"}]", out],
+    # :xml  => [Representable::XML,  out="<a><song></song><song></song></a>", out]
+  ) do |format, mod, output, input|
+
+    # singular is automatically detected.
+    describe "Module#to_/from_#{format}" do
+      let (:format) { format }
+
+      let (:representer) {
+        Module.new do
+          include mod
+          include Representable::Collection
+          property :name
+        end
+      }
+
+      # rendering works out of the box, no config necessary
+      it { render(song.extend(representer)).must_equal_document output }
+      it { parse(Song.new.extend(representer), input).must_equal song }
+    end
+
+
+    # describe "Decorator::for_collection [#{format}]" do
+    #   let (:format) { format }
+    #   let (:representer) {
+    #     Class.new(Representable::Decorator) do
+    #       include mod
+    #       property :name
+
+    #       collection_representer :class => Song
+    #     end
+    #   }
+
+    #   it { render(representer.for_collection.new(songs)).must_equal_document output }
+    #   it { parse(representer.for_collection.new([]), input).must_equal songs }
+    # end
+  end
+end
