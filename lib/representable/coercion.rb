d452c7527b8a14a16ff437b006362a674966b03b
Nick Sutterer <apotonick@gmail.com>
2014-07-29 15:35:37 +1000

    pass type into Coercer.

--- lib/representable/coercion.rb
+++ lib/representable/coercion.rb
@@ -3,10 +3,14 @@ require "virtus"
 module Representable
   module Coercion
     class Coercer
+      def initialize(type)
+        @type = type
+      end
+
       # This gets called when the :render_filter or :parse_filter option is evaluated.
       # Usually the Coercer instance is an element in a Pipeline to allow >1 filters per property.
       def call(value, doc, options)
-        Virtus::Attribute.build(options.binding[:type]).coerce(value)
+        Virtus::Attribute.build(@type).coerce(value)
       end
     end
 
@@ -23,9 +27,7 @@ module Representable
       def build_definition(name, options, &block) # Representable::Declarative
         return super unless type = options[:type]
 
-        options[:pass_options]  = true # TODO: remove, standard.
-
-        options[:render_filter] << coercer = Coercer.new
+        options[:render_filter] << coercer = Coercer.new(type)
         options[:parse_filter]  << coercer
 
         super
