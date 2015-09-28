3fc8e1cdac3e6803623aee9e8190bc995b0e4263
Nick Sutterer <apotonick@gmail.com>
2015-09-28 15:26:27 +1000

    first cleanup.

--- lib/representable/populator.rb
+++ lib/representable/populator.rb
@@ -16,7 +16,7 @@ module Representable
 
   Deserialize = ->(fragment, doc, binding) do
     # use a Deserializer to transform fragment to/into object.
-    binding.send(:populator).send(:deserializer).call(fragment)
+    binding.send(:deserializer).call(fragment)
   end
 
   ParseFilter = ->(value, doc, binding) do
@@ -26,50 +26,17 @@ module Representable
   Set = ->(value, doc, binding) do
     binding.set(value)
   end
-  #
-  # populator
-      # skip_parse? --> return
-      # deserialize (this is where additional logic can happen, e.g. Object-HAL's collection semantics).
-      # parse_filter
-      # set
-  class Populator # rename to Deserializer?
+
+  class Populator
     def initialize(binding)
       @binding = binding
     end
 
-
-
     # goal of this is to have this workflow apply-able to collections AND to items per collection, or for items in hashes.
     def call(fragment, doc)
-      return Pipeline[Default, SkipParse, Deserialize, ParseFilter, Set].("blaaaaaaa", fragment, doc, @binding)
-
-      # the rest should be applied per item (collection) or per fragment (collection and property)
-      if fragment == Binding::FragmentNotFound
-        return unless @binding.has_default?
-        value = @binding[:default]
-      else
-        # DISCUSS: should we return a Skip object instead of this block trick? (same in Binding#serialize?)
-        value = deserialize(fragment) { return } # stop here if skip_parse.
-      end
-
-      value = @binding.parse_filter(value, doc)
-        # parse_filter
-        # set
-      @binding.set(value)
+      Pipeline[Default, SkipParse, Deserialize, ParseFilter, Set].
+        ("blaaaaaaa", fragment, doc, @binding)
     end
-
-  private
-    def deserialize(fragment)
-      return yield if @binding.evaluate_option(:skip_parse, fragment) # TODO: move this into Deserializer.
-
-      # use a Deserializer to transform fragment to/into object.
-      deserializer.call(fragment) # CollectionDeserializer/HashDeserializer/etc.
-    end
-
-    def deserializer
-      @binding.deserializer
-    end
-
 
     # A separated collection deserializer/populator allows us better dealing with populating/modifying
     # collections of models. (e.g. replace, update, push, etc.).
