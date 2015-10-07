f8cd20aafdef61db0692c39a5584f14acd80da22
Nick Sutterer <apotonick@gmail.com>
2015-10-07 22:19:18 +1100

    defer :parse_pipeline and :render_pipeline to a later time, this is really icing the yummy cake.

--- test/---deserialize-pipeline_test.rb
+++ test/---deserialize-pipeline_test.rb
@@ -5,21 +5,6 @@ class DeserializePipelineTest < MiniTest::Spec
   Artist = Struct.new(:email)
   Song   = Struct.new(:title)
 
-  # this is gonna implement a simple pipeline that we can reuse across the entire gem.
-  # class ArtistPopulator
-  #   include Uber::Callable
-
-
-
-  #   def call(represented, fragment, options)
-  #     result  = fragment != Representable::Binding::FragmentNotFound
-  #     return unless result # this is one pipeline step.
-  #     # here, another step could be plugged in, e.g. :default.
-
-  #     represented.artist = options.binding.representer_module_for(nil).new(Artist.new).from_hash(fragment)
-  #   end
-  # end
-
   # tests [Collect[Instance, Prepare, Deserialize], Setter]
   class Representer < Representable::Decorator
     include Representable::Hash
@@ -43,6 +28,7 @@ class DeserializePipelineTest < MiniTest::Spec
   end
 
   it do
+    skip "TODO: implement :parse_pipeline and :render_pipeline, and before/after/replace semantics"
     album = Album.new
     Representer.new(album).from_hash({"artist"=>{"email"=>"yo"}, "songs"=>[{"title"=>"Affliction"}, {"title"=>"Dream Beater"}]})
     album.songs.must_equal([Song.new("Affliction"), Song.new("Dream Beater")])
