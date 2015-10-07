2829e5089c49588efc43fdffac2ca64410b0607b
Nick Sutterer <apotonick@gmail.com>
2015-10-08 09:33:12 +1100

    correct indentation.

--- lib/representable/pipeline_factories.rb
+++ lib/representable/pipeline_factories.rb
@@ -1,48 +1,46 @@
 # NOTE: this might become a separate class, that's why it's in a separate file.
 module Representable
   module Binding::Factories
-  def parse_functions
-    return self[:parse_pipeline].() if self[:parse_pipeline] # untested.
+    def parse_functions
+      return self[:parse_pipeline].() if self[:parse_pipeline]
+      if array?
+        return [*default_init_functions, Collect[*default_parse_fragment_functions], *default_post_functions]
+      end
+      if self[:hash] # FIXME: fuckin' merge with array?
+        return [*default_init_functions, Collect::Hash[*default_parse_fragment_functions], *default_post_functions]
+      end
 
-    if array?
-      return [*default_init_functions, Collect[*default_parse_fragment_functions], *default_post_functions]
-    end
-    if self[:hash] # FIXME: fuckin' merge with array?
-      return [*default_init_functions, Collect::Hash[*default_parse_fragment_functions], *default_post_functions]
-    end
-
-    [*default_init_functions, *default_parse_fragment_functions, *default_post_functions]
-  end
-
-
-  def render_functions
-    # return self[:parse_pipelinerender_pipeline].() if self[:render_pipeline] # untested. # FIXME.
-
-    if array?
-      return [*default_render_init_functions, StopOnSkipable, StopOnNil, Collect[*default_render_fragment_functions], WriteFragment]
+      [*default_init_functions, *default_parse_fragment_functions, *default_post_functions]
     end
 
-    if self[:hash]
-      return [*default_render_init_functions, StopOnSkipable, StopOnNil, Collect::Hash[*default_render_fragment_functions], WriteFragment]
+
+    def render_functions
+      if array?
+        return [*default_render_init_functions, StopOnSkipable, StopOnNil, Collect[*default_render_fragment_functions], WriteFragment]
+      end
+
+      if self[:hash]
+        return [*default_render_init_functions, StopOnSkipable, StopOnNil, Collect::Hash[*default_render_fragment_functions], WriteFragment]
+      end
+
+      [*default_render_init_functions, RenderDefault, StopOnSkipable, *default_render_fragment_functions, WriteFragment]
     end
 
-    [*default_render_init_functions, RenderDefault, StopOnSkipable, *default_render_fragment_functions, WriteFragment]
-  end
+    def default_render_fragment_functions
+      functions = []
 
-  def default_render_fragment_functions
-    functions = []
+      functions << SkipRender if self[:skip_render]
 
-    functions << SkipRender if self[:skip_render]
+      if typed?
+        functions << Prepare
+      end
+      functions << Serialize if representable?
 
-    if typed?
-      functions << Prepare
+      functions
     end
-    functions << Serialize if representable?
 
-    functions
-  end
-  def default_render_init_functions
-    functions = [Getter]
+    def default_render_init_functions
+      functions = [Getter]
       functions << Writer if self[:writer]
       functions << RenderFilter if self[:render_filter].any?
       functions
