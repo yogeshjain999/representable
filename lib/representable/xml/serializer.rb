549524f446a0c9868f4c0f51bd35ab6a8240b908
Nick Sutterer <apotonick@gmail.com>
2017-04-17 15:49:35 +0200

    add XML::Serializer, which is 100% faster than Nokogiri and has about 25 LOC so far.

---
+++ lib/representable/xml/serializer.rb
@@ -0,0 +1,39 @@
+module Representable
+  module XML
+    # Functions to create a node tree and render it to XML.
+    # Yes - things can be that simple!
+    #
+    module_function
+    def Document()
+      document = Node("%", {})
+      # document << Node("?xml", version: "1.0") #<?xml version="1.0"?>
+      document
+    end
+
+    # Create tree functions
+    def Node(name, attrs={}, string=nil, children=[])
+      [name, attrs, string, children]
+    end
+
+    def Append(parent, node)
+      parent.last << node
+      node
+    end
+
+    # Render tree functions
+    def Render(arr)
+      arr.collect { |(name, attrs, text, children)|
+        content = children.any? ? Render(children) : text # TODO: only children argument
+        RenderNode(name, attrs, content)
+         }.join{""}
+    end
+
+    def RenderNode(name, attrs, content)
+      %{<#{name}#{RenderAttributes(attrs)}>#{content}</#{name}>}
+    end
+
+    def RenderAttributes(attrs)
+      attrs.collect { |k, v| %{ #{k}="#{v}"} }.join("")
+    end
+  end
+end
